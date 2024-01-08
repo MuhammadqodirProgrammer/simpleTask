e610c599e63ef33d4fceedd8aa8a78b929b50552
MuhammadqodirProgrammer <115607323+MuhammadqodirProgrammer@users.noreply.github.com>
2024-01-08 22:09:33 +0500

    small commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export const Home = () => {
- return (
- <div>
-    Welcome to the home page ☺
- </div>
- )
-}
+	return (
+		<div>
+			<h1 style={{ color: 'teal', textAlign: 'center' }}>
+				Welcome to the home page ☺
+			</h1>
+		</div>
+	);
+};
